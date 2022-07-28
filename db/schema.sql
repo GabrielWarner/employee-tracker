8020688176748ef1f8eea55c020a10b74a95bc95
GabrielWarner <gabe0412@uw.edu>
2022-07-27 17:13:39 -0700

    schema working

--- db/schema.sql
+++ db/schema.sql
@@ -22,5 +22,10 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    FOREIGN KEY ()
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE CASCADE
 );
