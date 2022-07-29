eebb2d783d316bc32e5f9d236306576b4e598436
GabrielWarner <gabe0412@uw.edu>
2022-07-28 19:37:10 -0700

    added comments

--- index.js
+++ index.js
@@ -1,43 +1,37 @@
+//Gabriel Warner - Employee Tracker - 7/28/22
 //installing npm packages
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const cTable = require("console.table");
-
 //importing classes
-const Department = require('./lib/Department')
-const Role = require('./lib/Role')
-const Employee = require('./lib/Employee')
-
-
+const Department = require("./lib/Department");
+const Role = require("./lib/Role");
+const Employee = require("./lib/Employee");
 //creating connection to database
 const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      user: 'root',
-      password: 'password',
-      database: 'company_db'
-    },
-    console.log(`Connected to the movies_db database.`)
-    
-  );
-
-
+  {
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "company_db",
+  },
+  console.log(`Connected to the movies_db database.`)
+);
 db.connect((err) => {
-    if(err){
-        throw err
-    }
-
-})
+  if (err) {
+    throw err;
+  }
+});
 //creating objects that hold querys
-const department = new Department(db)
-const role = new Role(db)
-const employee = new Employee(db)
-
+const department = new Department(db);
+const role = new Role(db);
+const employee = new Employee(db);
 //function to run CLI
 const start = () => {
   inquirer
     .prompt([
       {
+        //listing out all options
         type: "list",
         choices: [
           "View All Employees",
@@ -54,64 +48,64 @@ const start = () => {
       },
     ])
     .then((ans) => {
+      //switch statement to collect user choice and then run function based off selection
       switch (ans.userChoice) {
-        
         case "View All Employees":
+          //uses employee Class method to get employees and then use console table to display results
           employee.getEmployees().then(([data]) => {
-            console.table(data)
-            start()
+            //displaying results
+            console.table(data);
+            //calling start function to ask the user questions again
+            start();
           });
           break;
-
         case "Add Employee":
           employee.addEmployee().then(([data]) => {
-            start()
-          })
-          break;
-
-        case "Update Employee Role":
-          employee.updateEmployee().then(([data]) => {
-            start()
-          })
-          break;
-
-        case "View All Roles":
-          role.getRoles().then(([data]) => {
-            console.table(data)
-            start()
+            start();
           });
           break;
-
-        case "Add Role":
-          role.addRole().then(([data])=>{
-            start()
-        })
+        //data is not being used because everything that is outputted is in the function
+        case "Update Employee Role":
+          employee.updateEmployee().then(([data]) => {
+            start();
+          });
           break;
-
+        //same process as above except using the Role Class
+        case "View All Roles":
+          role.getRoles().then(([data]) => {
+            console.table(data);
+            start();
+          });
+          break;
+        //same process as above except using the Role Class
+        case "Add Role":
+          role.addRole().then(([data]) => {
+            start();
+          });
+          break;
+        //same process as above except using the Department Class
         case "View All Departments":
           department.getDepartments().then(([data]) => {
-            console.table(data)
-            start()
+            console.table(data);
+            start();
           });
           break;
         case "Add Department":
           department.addDepartment().then(([rows, fields]) => {
-            console.log(rows)
-            start()
-        });
-          
+            console.log(rows);
+            start();
+          });
           break;
-
+        //option that ends the process
         case "Quit":
-          process.exit()
+          process.exit();
           break;
-
         default:
           console.log("thanks for playing!");
           break;
       }
-    })
+    });
 };
 
-
+//calling initial start function. This function runs in a loop because everytime the user chooses an option the start function gets called again
 start();
